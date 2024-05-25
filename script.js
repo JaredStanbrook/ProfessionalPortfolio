2df05dfe7f492b8af53f154eb95e9379c3796f79
Jared Stanbrook <Jared.Stanbrook@outlook.com>
2024-05-25 10:58:28 +0800

    Update

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     var cursor = document.querySelector(".blob");
+    var btns = document.getElementsByClassName("group");
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            var current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        });
+    }
     document.addEventListener("mousemove", function (e) {
-        var x = e.clientX;
-        var y = e.clientY;
-        cursor.style.display = "flex";
+        var w = window.innerWidth;
+        //change! very bad performace!
+        if (!(w <= 1000)) {
+            cursor.style.display = "flex";
+        } else {
+            cursor.style.display = "none";
+        }
         cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
     });
 });
