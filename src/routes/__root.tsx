0deea6bf51dd49ce922c3450cfce8f1162008411
Jared Stanbrook <22810943+JaredStanbrook@users.noreply.github.com>
2025-05-21 19:25:56 +0800

    style: update navbar styling for improved layout and appearance
    - Enhanced navbar spacing, alignment, and responsiveness.
    - Updated button and icon styles for better visual clarity.
    - Improved theme toggle placement and overall navbar aesthetics.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -19,13 +19,15 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 function Root() {
   return (
     <div className="bg-background leading-relaxed text-foreground antialiased">
-      <Link to={"/navigation"} className="fixed top-4 left-4 z-50">
-        <Button variant="ghost" size="icon">
-          <Menu />
-        </Button>
-      </Link>
-      <div className="fixed top-4 right-4 z-50">
-        <ModeToggle />
+      <div className="fixed flex flex-row gap-2 top-2.5 right-4 lg:w-full lg:flex-row lg:justify-between lg:right-0 lg:px-5 lg:top-4 z-50">
+        <Link to={"/navigation"} className="lg:order-first">
+          <Button variant="ghost" size="icon">
+            <Menu />
+          </Button>
+        </Link>
+        <div className="lg:order-last">
+          <ModeToggle />
+        </div>
       </div>
       <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0">
         <Outlet />
